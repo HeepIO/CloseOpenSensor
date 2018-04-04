2b12074bdcb62979deb92a7a3aee778010d34a39
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-04-04 07:12:02 -0400

    Created close open sensor repo

---
+++ CloseOpenSensor/CloseOpenSensor.ino
@@ -0,0 +1,31 @@
+
+#include "HeepDeviceDefinitions.h"
+
+#define OPEN_PIN 14
+
+void InitializeControlHardware(){
+  pinMode(OPEN_PIN,INPUT);
+}
+
+int ReadOpen(){
+  int currentSetting = digitalRead(OPEN_PIN);
+  SetControlValueByName("Open",currentSetting);
+  return currentSetting;
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  InitializeControlHardware();
+  AddOnOffControl("Open",HEEP_OUTPUT,0);
+  StartHeep("CloseOpenSensor", 6);
+
+}
+
+void loop()
+{
+  PerformHeepTasks();
+  ReadOpen();
+
+}
